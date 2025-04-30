ed3b421d3a505b4dab27eeeb7539c43708a6e3e0
Timur Khvat <timurkhvat@gmail.com>
2025-04-30 17:44:38 +0200

    'Solution'

--- taxi/admin.py
+++ taxi/admin.py
@@ -16,4 +16,5 @@ class DriverAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number",)}),)
     add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("license_number",)}),)
 
+
 admin.site.register(Manufacturer)
